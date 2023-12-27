95430df720a5cc5834006d075495a2026b707ae4
JosaelPrz <josael.prz@gmail.com>
2023-12-27 06:39:21 +0000

    Fix naming conventions and typos on tests

--- tests/test_cropping.py
+++ tests/test_cropping.py
@@ -8,7 +8,7 @@ import pytest
     (0, 0),
     (-1, -1)
 ], ids=["positive", "zero", "negative"])
-def test_pass_valid_point(x: int, y: int):
+def test_pass_point(x: int, y: int):
     point = Point(x=x, y=y)
     assert point.x == x
     assert point.y == y
@@ -19,7 +19,7 @@ def test_pass_valid_point(x: int, y: int):
     (0, 0),
     (-1, -1)
 ], ids=["positive", "zero", "negative"])
-def test_pass_valid_dimension(width: int, height: int):
+def test_pass_dimension(width: int, height: int):
     dimension = Dimension(width=width, height=height)
     assert dimension.width == width
     assert dimension.height == height
@@ -45,7 +45,7 @@ def test_pass_cropping_configuration_origin(x: int, y: int):
     assert configuration.origin == origin
 
 
-def test_fail_negative_origin():
+def test_fail_cropping_configuration_negative_origin():
     origin = Point(x=-1, y=-1)
     dimensions = Dimension(width=100, height=100)
     aspect_ratio = AspectRatio.FREE
@@ -55,7 +55,7 @@ def test_fail_negative_origin():
             origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
 
 
-def test_fail_free_cropping_configuration_to_small_dimensions():
+def test_fail_free_cropping_configuration_too_small_dimensions():
     origin = Point(x=0, y=0)
     dimensions = Dimension(width=10, height=10)
     aspect_ratio = AspectRatio.FREE
