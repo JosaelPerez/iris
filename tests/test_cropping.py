85db1f5981388cefe31b4b014b0a9d4112757898
JosaelPrz <josael.prz@gmail.com>
2023-12-27 05:12:42 +0000

    Add positive origin pass test

---
+++ tests/test_cropping.py
@@ -0,0 +1,13 @@
+from app.iris.effects import CroppingConfiguration
+from app.iris.utils import Point, Dimension, AspectRatio
+
+
+def test_pass_positive_origin():
+    origin = Point(x=100, y=100)
+    dimensions = Dimension(width=100, height=100)
+    aspect_ratio = AspectRatio.FREE
+
+    configuration = CroppingConfiguration(origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+    assert configuration.origin == origin
+
+
