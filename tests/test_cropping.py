150f12705262ca2f9ddcddb60afa8d7a50380743
JosaelPrz <josael.prz@gmail.com>
2023-12-27 06:23:36 +0000

    Add default cropping configuration with height fail test

--- tests/test_cropping.py
+++ tests/test_cropping.py
@@ -2,56 +2,68 @@ from app.iris.effects import CroppingConfiguration
 from app.iris.utils import Point, Dimension, AspectRatio
 import pytest
 
+
 def test_pass_positive_point():
     point = Point(x=100, y=100)
     assert point.x == 100
     assert point.y == 100
+
 
 def test_pass_zero_point():
     point = Point(x=0, y=0)
     assert point.x == 0
     assert point.y == 0
 
+
 def test_pass_negative_point():
     point = Point(x=-1, y=-1)
     assert point.x == -1
     assert point.y == -1
+
 
 def test_pass_positive_dimension():
     dimension = Dimension(width=100, height=100)
     assert dimension.width == 100
     assert dimension.height == 100
 
+
 def test_pass_zero_dimension():
     dimension = Dimension(width=0, height=0)
     assert dimension.width == 0
     assert dimension.height == 0
+
 
 def test_pass_negative_dimension():
     dimension = Dimension(width=-1, height=-1)
     assert dimension.width == -1
     assert dimension.height == -1
 
+
 def test_pass_default_height_dimension():
     dimension = Dimension(width=100)
     assert dimension.width == 100
     assert dimension.height == 0
+
 
 def test_pass_positive_origin():
     origin = Point(x=100, y=100)
     dimensions = Dimension(width=100, height=100)
     aspect_ratio = AspectRatio.FREE
 
-    configuration = CroppingConfiguration(origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+    configuration = CroppingConfiguration(
+        origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
     assert configuration.origin == origin
+
 
 def test_pass_zero_origin():
     origin = Point(x=0, y=0)
     dimensions = Dimension(width=100, height=100)
     aspect_ratio = AspectRatio.FREE
 
-    configuration = CroppingConfiguration(origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+    configuration = CroppingConfiguration(
+        origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
     assert configuration.origin == origin
+
 
 def test_fail_negative_origin():
     origin = Point(x=-1, y=-1)
@@ -59,7 +71,9 @@ def test_fail_negative_origin():
     aspect_ratio = AspectRatio.FREE
 
     with pytest.raises(ValueError):
-        CroppingConfiguration(origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+        CroppingConfiguration(
+            origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+
 
 def test_fail_free_cropping_configuration_to_small_dimensions():
     origin = Point(x=0, y=0)
@@ -67,7 +81,9 @@ def test_fail_free_cropping_configuration_to_small_dimensions():
     aspect_ratio = AspectRatio.FREE
 
     with pytest.raises(ValueError):
-        CroppingConfiguration(origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+        CroppingConfiguration(
+            origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+
 
 def test_fail_free_cropping_configuration_with_zero_dimensions():
     origin = Point(x=0, y=0)
@@ -75,4 +91,22 @@ def test_fail_free_cropping_configuration_with_zero_dimensions():
     aspect_ratio = AspectRatio.FREE
 
     with pytest.raises(ValueError):
-        CroppingConfiguration(origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+        CroppingConfiguration(
+            origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
+
+
+@pytest.mark.parametrize("aspect_ratio", [
+    AspectRatio.SQUARE,
+    AspectRatio.THREE_TWO,
+    AspectRatio.FOUR_THREE,
+    AspectRatio.FIVE_FOUR,
+    AspectRatio.SIXTEEN_NINE,
+    AspectRatio.SIXTEEN_TEN
+])
+def test_fail_default_cropping_configuration_with_height(aspect_ratio: AspectRatio):
+    origin = Point(x=0, y=0)
+    dimensions = Dimension(width=100, height=100)
+
+    with pytest.raises(ValueError):
+        CroppingConfiguration(
+            origin=origin, dimensions=dimensions, aspect_ratio=aspect_ratio)
