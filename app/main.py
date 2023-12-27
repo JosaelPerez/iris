e28d71b53719f7540f3f5d01f9f749955f98a5f9
JosaelPrz <josael.prz@gmail.com>
2023-12-27 04:43:03 +0000

    Add debugging code to main file

--- app/main.py
+++ app/main.py
@@ -8,3 +8,9 @@ app.include_router(effects.router)
 @app.get("/")
 async def root():
     return {"data": "Blackcore Iris API"}
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == '__main__':
+    main()
